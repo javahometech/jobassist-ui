25252f59a2ada7449dcb8f9c1ca53edd5888ad6e
Hari Kammana <hari.kammana@gmail.com>
2018-07-21 18:28:35 +0530

    jobassist-cognito

---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+export const environment = {
+    production: false,
+
+    region: 'ap-south-1',
+
+    identityPoolId: 'ap-south-1_bFQRaxGZy',
+    userPoolId: 'ap-south-1_uT5JYipKb',
+    clientId: '2qf68eljmsvcg9oimv2apun50g',
+
+    rekognitionBucket: 'rekognition-pics',
+    albumName: "usercontent",
+    bucketRegion: 'us-east-1',
+
+    ddbTableName: 'LoginTrail',
+
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
+
+};
